1b2c13e1cdef74eb47f81f535ca67a3acd79a40f
JoshTakchi <josh.takchi@gmail.com>
2023-01-30 20:31:39 +1100

    edit

--- modulenotebooks/test.py
+++ modulenotebooks/test.py
@@ -1,7 +1,14 @@
 import time
+import math
 
 def rand_func(num):
     print(f'First function sleeping for {num} second(s)')
     time.sleep(num)
     return f'Completed first function in {num} second(s)'
+
+
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0: return False
+    return True
 
